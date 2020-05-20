49b4ce8d5e42544bfe4f23ce66f6dceb9df8ded7
Jason Gorman <jason.gorman@codemanship.com>
2020-05-20 11:50:03 +0100

    Completed tests for Mars Rover

--- test/rover_test.js
+++ test/rover_test.js
@@ -1,31 +1,61 @@
 const assert = require('assert');
 
 function go(rover, instructions) {
-    if(instructions == 'R'){
-        if(rover.facing == 'N')
-            return {...rover, facing: "E"};
 
-        if(rover.facing == 'E')
-            return {...rover, facing: 'S'};
+    return Array.from(instructions).reduce((rover, instruction) => {
+        if(instruction == 'R'){
+            if(rover.facing == 'N')
+                return {...rover, facing: "E"};
 
-        if(rover.facing == 'S')
-            return {...rover, facing: 'W'};
+            if(rover.facing == 'E')
+                return {...rover, facing: 'S'};
 
-        return {...rover, facing: 'N'};
-    };
+            if(rover.facing == 'S')
+                return {...rover, facing: 'W'};
 
-    if(instructions == 'L'){
-        if(rover.facing == 'N')
-            return {...rover, facing: "W"};
+            return {...rover, facing: 'N'};
+        };
 
-        if(rover.facing == 'W')
-            return {...rover, facing: 'S'};
+        if(instruction == 'L'){
+            if(rover.facing == 'N')
+                return {...rover, facing: "W"};
 
-        if(rover.facing == 'S')
-            return {...rover, facing: 'E'};
+            if(rover.facing == 'W')
+                return {...rover, facing: 'S'};
 
-        return {...rover, facing: 'N'};
-    }
+            if(rover.facing == 'S')
+                return {...rover, facing: 'E'};
+
+            return {...rover, facing: 'N'};
+        }
+
+        if(instruction == 'F'){
+            if(rover.facing == 'N')
+                return {...rover, position: {...rover.position, y: rover.position.y + 1}};
+
+            if(rover.facing == 'E')
+                return {...rover, position: {...rover.position, x: rover.position.x + 1}};
+
+            if(rover.facing == 'S')
+                return {...rover, position: {...rover.position, y: rover.position.y - 1}};
+
+            return {rover, position: {...rover.position, x: rover.position.x - 1}};
+        };
+
+        if(instruction == 'B'){
+            if(rover.facing == 'N')
+                return {...rover, position: {...rover.position, y: rover.position.y - 1}};
+
+            if(rover.facing == 'E')
+                return {...rover, position: {...rover.position, x: rover.position.x - 1}};
+
+            if(rover.facing == 'S')
+                return {...rover, position: {...rover.position, y: rover.position.y + 1}};
+
+            return {rover, position: {...rover.position, x: rover.position.x + 1}};
+        }
+    }, rover);
+
 
 }
 
@@ -77,4 +107,58 @@ describe('Mars Rover', () => {
         rover = go(rover, "L");
         assert.equal(rover.facing, 'N');
     });
+
+    it('Moves forward Y+1 facing N', () => {
+        let rover = {facing: 'N', position: {x: 5, y: 5}};
+        rover = go(rover, 'F');
+        assert.deepEqual(rover.position, {x: 5, y:6});
+    });
+
+    it('Moves forward X+1 facing E', () => {
+        let rover = {facing: 'E', position: {x: 5, y: 5}};
+        rover = go(rover, 'F');
+        assert.deepEqual(rover.position, {x: 6, y:5});
+    });
+
+    it('Moves forward Y-1 facing S', () => {
+        let rover = {facing: 'S', position: {x: 5, y: 5}};
+        rover = go(rover, 'F');
+        assert.deepEqual(rover.position, {x: 5, y:4});
+    });
+
+    it('Moves forward X-1 facing W', () => {
+        let rover = {facing: 'W', position: {x: 5, y: 5}};
+        rover = go(rover, 'F');
+        assert.deepEqual(rover.position, {x: 4, y:5});
+    });
+
+    it('Moves back Y-1 facing N', () => {
+        let rover = {facing: 'N', position: {x: 5, y: 5}};
+        rover = go(rover, 'B');
+        assert.deepEqual(rover.position, {x: 5, y:4});
+    });
+
+    it('Moves back X-1 facing E', () => {
+        let rover = {facing: 'E', position: {x: 5, y: 5}};
+        rover = go(rover, 'B');
+        assert.deepEqual(rover.position, {x: 4, y:5});
+    });
+
+    it('Moves back Y+1 facing S', () => {
+        let rover = {facing: 'S', position: {x: 5, y: 5}};
+        rover = go(rover, 'B');
+        assert.deepEqual(rover.position, {x: 5, y:6});
+    });
+
+    it('Moves back X+1 facing W', () => {
+        let rover = {facing: 'W', position: {x: 5, y: 5}};
+        rover = go(rover, 'B');
+        assert.deepEqual(rover.position, {x: 6, y:5});
+    });
+
+    it('Executes sequence of instructions', () => {
+        let rover = {facing: 'N', position: {x: 5, y: 5}};
+        rover = go(rover, 'RFF');
+        assert.deepEqual(rover, {facing: 'E', position: {x: 7, y: 5}});
+    })
 })
